59661d0f8e07cd8dc9ae939f0dd14872c159d4b0
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-04-17 17:42:40 +0300

    add more typedefs and mutations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 //Scalar types - String, Boolean, Int, Float, ID
 //Demo user data
-const comments = [{
+let comments = [{
         id: "1",
         text: "This is a comment",
         author: "1",
@@ -46,7 +47,7 @@ const users = [{
     }
 ]
 
-const posts = [{
+let posts = [{
         id: "1",
         title: "My first post",
         body: "This is my first post",
@@ -85,6 +86,32 @@ const typeDefs = `
         users(query:String): [User!]!
         me: User!
         post: Post!
+    }
+
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+        deleteUser(id: ID!): User!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -144,6 +171,65 @@ const resolvers = {
                 body: "This is my first post",
                 published: true
             }
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.data.email);
+            if (emailTaken) {
+                throw new Error("Email taken!");
+            }
+            const user = {
+                id: uuidv4(),
+                ...args.data
+            }
+            users.push(user);
+            return user;
+        },
+        createPost(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.data.author);
+            if (!userExists) {
+                throw new Error("User not found!");
+            }
+            const post = {
+                id: uuidv4(),
+                ...args.data
+            }
+            posts.push(post);
+            return post;
+        },
+        createComment(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.data.author);
+            const postExists = posts.some((post) => post.id === args.data.post && post.published);
+            if (!userExists || !postExists) {
+                throw new Error("Unable to find user or post!");
+            }
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            }
+            comments.push(comment);
+            return comment;
+        },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex((user) => user.id === args.id);
+            if (userIndex === -1) {
+                throw new Error("User not found!");
+            }
+            const deletedUsers = users.splice(userIndex, 1);
+
+            posts = posts.filter((post) => {
+                const match = post.author === args.id;
+
+                if (match) {
+                    comments = comments.filter((comment) => comment.post !== post.id);
+                }
+
+                return !match;
+            })
+            comments = comments.filter((comment) => comment.author !== args.id);
+
+            return deletedUsers[0];
         }
     },
     Post: {
