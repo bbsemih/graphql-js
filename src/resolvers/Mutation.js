bef884373a780ec2ce80d5526511b35a4a7c165d
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2023-04-19 18:02:41 +0300

    add comment subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -42,7 +42,7 @@ const Mutation = {
         }
         return post;
     },
-    createComment(parent, args, { db }, info) {
+    createComment(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author);
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published);
         if (!userExists || !postExists) {
@@ -53,6 +53,8 @@ const Mutation = {
             ...args.data
         }
         db.comments.push(comment);
+
+        pubsub.publish(`comment ${args.data.post}`, { comment })
         return comment;
     },
     deleteUser(parent, args, { db }, info) {
